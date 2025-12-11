6bb86e29bff47336fb5f0dcb8ded94371b4c8fc9
shakthi <shakthisravanth001@gmail.com>
2025-12-11 12:27:53 +0530

    lab-5 added

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
+<<<<<<< HEAD
    public static void main(String[] args) {
         // write your program here
+=======
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Add: " + (a + b));
+        System.out.println("Sub: " + (a - b));
+        System.out.println("Mul: " + (a * b));
+        System.out.println("Div: " + (a / b));
+
+        a += 5; // assignment operator
+        System.out.println("After a += 5: " + a);
+>>>>>>> e743d32 (calculater test)
     }
 }

45ccbaa1123b702f5e451446687d1457ef9ace89
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2025-12-11 10:31:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,5 @@
 public class Main {
    public static void main(String[] args) {
-        String name = "Rahul";
-        int age = 20;
-        double cgpa = 8.7;
-        char grade = 'A';
-        boolean isPlaced = false;
-
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("CGPA: " + cgpa);
-        System.out.println("Grade: " + grade);
-        System.out.println("Placed? " + isPlaced);
+        
     }
 }

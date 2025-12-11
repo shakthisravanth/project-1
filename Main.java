0d8600bbab11abb81a6dd1a7d9d010fefdaa488d
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2025-12-11 10:23:17 +0530

    Update main method with user details

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        String name = "";
+   public static void main(String[] args) {
+        String name = "Rahul";
+        int age = 20;
+        double cgpa = 8.7;
+        char grade = 'A';
+        boolean isPlaced = false;
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed? " + isPlaced);
     }
 }
